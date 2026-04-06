d6365b1d646e8ab728cb4f3450db4d16fb771ed9
Mike12138210 <3192742021@qq.com>
2026-04-06 21:13:07 +0800

    更新学习笔记，进度记录

--- backend/src/main/java/com/Mike12138210/SmartNote/dto/LoginRequest.java
+++ backend/src/main/java/com/Mike12138210/SmartNote/dto/LoginRequest.java
@@ -9,19 +9,11 @@ public class LoginRequest {
     @NotBlank(message = "密码不能为空")
     private String password;
 
-    public String getPassword() {
-        return password;
-    }
+    public String getPassword() {return password;}
 
-    public void setPassword(String password) {
-        this.password = password;
-    }
+    public void setPassword(String password) {this.password = password;}
 
-    public String getAccount() {
-        return account;
-    }
+    public String getAccount() {return account;}
 
-    public void setAccount(String account) {
-        this.account = account;
-    }
+    public void setAccount(String account) {this.account = account;}
 }
--- backend/src/main/java/com/Mike12138210/SmartNote/entity/Note.java
+++ backend/src/main/java/com/Mike12138210/SmartNote/entity/Note.java
@@ -23,91 +23,47 @@ public class Note {
     @TableLogic
     private Integer deleted;
 
-    public Long getId() {
-        return id;
-    }
+    public Long getId() {return id;}
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+    public void setId(Long id) {this.id = id;}
 
-    public Long getUserId() {
-        return userId;
-    }
+    public Long getUserId() {return userId;}
 
-    public void setUserId(Long userId) {
-        this.userId = userId;
-    }
+    public void setUserId(Long userId) {this.userId = userId;}
 
-    public String getTitle() {
-        return title;
-    }
+    public String getTitle() {return title;}
 
-    public void setTitle(String title) {
-        this.title = title;
-    }
+    public void setTitle(String title) {this.title = title;}
 
-    public String getContent() {
-        return content;
-    }
+    public String getContent() {return content;}
 
-    public void setContent(String content) {
-        this.content = content;
-    }
+    public void setContent(String content) {this.content = content;}
 
-    public String getTags() {
-        return tags;
-    }
+    public String getTags() {return tags;}
 
-    public void setTags(String tags) {
-        this.tags = tags;
-    }
+    public void setTags(String tags) {this.tags = tags;}
 
-    public String getPermission() {
-        return permission;
-    }
+    public String getPermission() {return permission;}
 
-    public void setPermission(String permission) {
-        this.permission = permission;
-    }
+    public void setPermission(String permission) {this.permission = permission;}
 
-    public String getAiSummary() {
-        return aiSummary;
-    }
+    public String getAiSummary() {return aiSummary;}
 
-    public void setAiSummary(String aiSummary) {
-        this.aiSummary = aiSummary;
-    }
+    public void setAiSummary(String aiSummary) {this.aiSummary = aiSummary;}
 
-    public String getAiKeyPoints() {
-        return aiKeyPoints;
-    }
+    public String getAiKeyPoints() {return aiKeyPoints;}
 
-    public void setAiKeyPoints(String aiKeyPoints) {
-        this.aiKeyPoints = aiKeyPoints;
-    }
+    public void setAiKeyPoints(String aiKeyPoints) {this.aiKeyPoints = aiKeyPoints;}
 
-    public LocalDateTime getCreateTime() {
-        return createTime;
-    }
+    public LocalDateTime getCreateTime() {return createTime;}
 
-    public void setCreateTime(LocalDateTime createTime) {
-        this.createTime = createTime;
-    }
+    public void setCreateTime(LocalDateTime createTime) {this.createTime = createTime;}
 
-    public LocalDateTime getUpdateTime() {
-        return updateTime;
-    }
+    public LocalDateTime getUpdateTime() {return updateTime;}
 
-    public void setUpdateTime(LocalDateTime updateTime) {
-        this.updateTime = updateTime;
-    }
+    public void setUpdateTime(LocalDateTime updateTime) {this.updateTime = updateTime;}
 
-    public Integer getDeleted() {
-        return deleted;
-    }
+    public Integer getDeleted() {return deleted;}
 
-    public void setDeleted(Integer deleted) {
-        this.deleted = deleted;
-    }
+    public void setDeleted(Integer deleted) {this.deleted = deleted;}
 }
